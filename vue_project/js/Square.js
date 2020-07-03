02d645f5e43ab476b50ba432ef6f9d288eedde3c
MarLMazo <marlmazo01@gmail.com>
2020-07-03 09:51:17 -0400

    Added comments and Citations on every files

--- vue_project/js/Square.js
+++ vue_project/js/Square.js
@@ -1,4 +1,5 @@
 class Square{
+    //Reference of this code is from DOm in Australia https://www.youtube.com/watch?v=rqb4FgVNrrM
     //This class only creates the squares and initialize the values of each square
     constructor(){
         this.value = null;
