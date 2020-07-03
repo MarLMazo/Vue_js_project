b2f161c67eaccdda9183f5a496c1b8a203ecc3d3
MarLMazo <marlmazo01@gmail.com>
2020-07-03 08:24:56 -0400

    initial commit

---
+++ vue_project/js/Square.js
@@ -0,0 +1,7 @@
+class Square{
+    //This class only creates the squares and initialize the values of each square
+    constructor(){
+        this.value = null;
+        this.isHighlighted = false;      
+    }
+}
